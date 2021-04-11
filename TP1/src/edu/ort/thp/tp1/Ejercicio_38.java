d26b1847ffe8ecab79580c420816e187ef45ea7f
facundosr <facundosr@gmail.com>
2021-04-11 16:38:23 -0300

    se valido la contraseña

--- TP1/src/edu/ort/thp/tp1/Ejercicio_38.java
+++ TP1/src/edu/ort/thp/tp1/Ejercicio_38.java
@@ -3,35 +3,39 @@ package edu.ort.thp.tp1;
 import java.util.Scanner;
 
 public class Ejercicio_38 {
+
 	private static Scanner input = new Scanner(System.in);
+
+	private static final String USUARIO = "admin";
+	private static final String PASSWORD = "123456";
+
 	public static void main(String[] args) {
-		String usuario = "admin";
-		int password = 123456;
+
 		String usuarioIngresado = "";
-		int passwordIngresado = 0;
+		String passwordIngresado = "";
 		boolean entrada = false;
 		int intentos = 3;
-		
+
 		do {
 			System.out.println("Ingrese el usuario");
 			usuarioIngresado = input.nextLine();
-			
 			System.out.println("Ingrese la contraseña");
-			passwordIngresado = Integer.parseInt(input.nextLine());
-			
-			if (usuario.equals(usuarioIngresado) && passwordIngresado == password) {
+			passwordIngresado = input.nextLine();
+
+			if (USUARIO.equals(usuarioIngresado) && PASSWORD.equals(passwordIngresado)) {
 				entrada = true;
-			} else {	
+			} else {
 				intentos -= 1;
 				System.out.println("Usuario o contraseña incorrecta, le quedan " + intentos + " intentos");
-			}			
-		} while(entrada == false && intentos > 0);
-		
-		if(entrada == true) {
+			}
+		} while (entrada == false && intentos > 0);
+
+		if (entrada == true) {
 			System.out.println("Acceso condedido");
 		} else {
 			System.out.println("Se ha bloqueado el acceso");
 		}
+		input.close();
 	}
 
 }
