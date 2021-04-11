5cd7c872f9f84ce75d51ec97ac568f6b4c16fa4c
facundosr <facundosr@gmail.com>
2021-04-11 17:41:06 -0300

    agregado 37

---
+++ TP1/src/edu/ort/thp/tp1/Ejercicio_37.java
@@ -0,0 +1,30 @@
+package edu.ort.thp.tp1;
+
+import java.util.Scanner;
+
+public class Ejercicio_37 {
+
+	private static final int CANT_SUELDOS_MAXIMA = 12;
+
+	private static Scanner input = new Scanner(System.in);
+
+	public static void main(String[] args) {
+
+		double sueldo = 0;
+		int contador = 0;
+		double sumaSueldos = 0;
+
+		do {
+			System.out.println("Ingrese el sueldo nro: " + (contador + 1));
+			sueldo = Integer.parseInt(input.nextLine());
+
+			if (sueldo > 0) {
+				sumaSueldos += sueldo;
+				contador++;
+			}
+
+		} while (contador < CANT_SUELDOS_MAXIMA && sueldo > 0);
+		System.out.println("La suma de los " + contador + " sueldos ingresados da " + sumaSueldos);
+	}
+
+}
